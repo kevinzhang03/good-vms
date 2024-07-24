6f7882a2d471bb8eda28a830de38fed74bf2249a
kevinzhangTT <kevin9019888@gmail.com>
2024-07-24 14:32:35 -0400

    eslinting and rename request component on publication

--- apps/web/src/components/Publication/Request/index.tsx
+++ apps/web/src/components/Publication/Request/index.tsx
@@ -8,13 +8,13 @@ import { getAuthApiHeaders } from '@helpers/getAuthApiHeaders';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-import Request from './Request';
+import RequestFields from './Request';
 
 interface SnapshotProps {
   id: string;
 }
 
-const Poll: FC<SnapshotProps> = ({ id }) => {
+const Request: FC<SnapshotProps> = ({ id }) => {
   const getRequest = async (): Promise<null | TRequest> => {
     try {
       const response = await axios.get(`${GOOD_API_URL}/requests/get`, {
@@ -49,7 +49,7 @@ const Poll: FC<SnapshotProps> = ({ id }) => {
     return null;
   }
 
-  return <Request request={data} refetch={refetch} />;
+  return <RequestFields refetch={refetch} request={data} />;
 };
 
-export default Poll;
+export default Request;
